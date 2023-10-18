9530775998dc87654c1ca4052e6dfafac84b0092
Miguel Guevara <miguel_g1789@hotmail.com>
2023-10-18 13:59:22 -0500

    continue laptop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,63 +3,63 @@ import { IoCall, IoMail, IoLocationSharp } from "react-icons/io5";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#024F3C] py-14 px-6">
-      <section className="flex gap-5 px-0 pb-8 border-b border-[#81fdde33]">
-        <Link to="/">
-          <p className="text-[#81FDDE]">Inicio</p>
-        </Link>
+    <footer className="bg-[#024F3C] py-14">
+      <div className="w-[87%] mx-auto">
+        <section className="flex gap-5 px-0 pb-8 border-b border-[#81fdde33]">
+          <Link to="/">
+            <p className="text-[#81FDDE]">Inicio</p>
+          </Link>
 
-        <Link to="/nosotros">
-          <p className="text-[#81FDDE]">Nosotros</p>
-        </Link>
+          <Link to="/nosotros">
+            <p className="text-[#81FDDE]">Nosotros</p>
+          </Link>
 
-        <p className="text-[#81FDDE]">Programas</p>
+          <Link to="/contacto">
+            <p className="text-[#81FDDE]">Contacto</p>
+          </Link>
+        </section>
 
-        <Link to="/contacto">
-          <p className="text-[#81FDDE]">Contacto</p>
-        </Link>
-      </section>
+        <section className="pt-14 pb-[70px] text-white flex flex-col gap-5">
+          <h3 className="pb-2.5 text-2xl font-semibold">Contacto</h3>
 
-      <section className="pt-14 pb-[70px] text-white flex flex-col gap-5">
-        <h3 className="pb-2.5 text-2xl font-semibold">Contacto</h3>
+          <div className="flex flex-col gap-6">
+            <div className="flex items-center gap-4">
+              <IoLocationSharp />
+              <span>Dirección</span>
+            </div>
 
-        <div className="flex flex-col gap-6">
-          <div className="flex items-center gap-4">
-            <IoLocationSharp />
-            <span>Dirección</span>
+            <div className="flex items-center gap-4">
+              <IoCall />
+              <span>+51 931130017</span>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <IoMail />
+              <span>hola@susanaferrel.com</span>
+            </div>
+          </div>
+        </section>
+
+        <section className="text-white flex md:flex-row-reverse flex-col md:items-center justify-between gap-9">
+          <div className="">
+            <p>©2023 Susana Ferrel - Derechos reservados</p>
           </div>
 
-          <div className="flex items-center gap-4">
-            <IoCall />
-            <span>+51 931130017</span>
-          </div>
+          <div className="flex gap-4 [&>div]:bg-[#128266] [&>div]:rounded-full [&>div]:p-3 [&>div>img]:w-5 [&>div>img]:h-5">
+            <div>
+              <img src="/images/linkedin.svg" />
+            </div>
 
-          <div className="flex items-center gap-4">
-            <IoMail />
-            <span>hola@susanaferrel.com</span>
-          </div>
-        </div>
-      </section>
+            <div>
+              <img src="/images/instagram.svg" />
+            </div>
 
-      <section className="text-white pt-[25px]">
-        <div className="pt-[1.79px] pb-[3.41px] h-20">
-          <p>©2023 Susana Ferrel - Derechos reservados</p>
-        </div>
-
-        <div className="flex gap-4 [&>div]:bg-[#128266] [&>div]:rounded-full [&>div]:p-3 [&>div>img]:w-5 [&>div>img]:h-5">
-          <div>
-            <img src="/images/linkedin.svg" />
+            <div>
+              <img src="/images/facebook.svg" />
+            </div>
           </div>
-
-          <div>
-            <img src="/images/instagram.svg" />
-          </div>
-
-          <div>
-            <img src="/images/facebook.svg" />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </footer>
   );
 };
--- src/views/home/page/Home.jsx
+++ src/views/home/page/Home.jsx
@@ -1,6 +1,7 @@
 import Button from "../../../components/Button";
 import Founder from "../../../components/Founder";
 import Partners from "../../../components/Partners";
+import Subtitles from "../../../components/Subtitles";
 import Collaborators from "../components/Collaborators";
 import Header from "../components/Header";
 
@@ -21,14 +22,19 @@ const Home = () => {
         <Partners />
       </div>
 
-      <div className="px-6 text-[#024F3C] flex flex-col items-center gap-10 mb-24">
-        <h2 className="text-center text-4xl font-bold">
-          Conviértete en la mejor versión de ti
-        </h2>
+      <div className="flex flex-col w-[87%] mx-auto items-center text-center lg:mb-40 mb-24">
+        <div className="mb-9">
+          <Subtitles>
+            Conviértete en la
+            <br />
+            mejor versión de ti
+          </Subtitles>
+        </div>
 
-        <p className="text-center text-[#181818]">
-          Con nosotros libera y potencia al máximo tus habilidades para lograr
-          tu mejor versión.
+        <p className="md:mb-16 mb-14">
+          Con nosotros libera y potencia al máximo tus
+          <br />
+          habilidades para lograr tu mejor versión.
         </p>
 
         <Button>Empieza tu consultoría</Button>
