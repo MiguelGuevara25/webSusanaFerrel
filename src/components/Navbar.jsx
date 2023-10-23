41cd7714dee13b4b8b88a926b45bedc446b1a6e4
Miguel Guevara <miguel_g1789@hotmail.com>
2023-10-23 18:19:27 -0500

    page completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,12 @@ const Navbar = () => {
 
   const openModal = () => {
     setIsOpen(true);
+    document.body.classList.add('no-scroll');
   };
 
   const closeModal = () => {
     setIsOpen(false);
+    document.body.classList.remove('no-scroll');
   };
 
   return (
