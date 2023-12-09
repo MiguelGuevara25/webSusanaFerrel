1310888a2af15c97720ae1596dee5fc21ef5004c
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-12-09 01:20:27 -0500

    change photo and background view home! !

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
   useEffect(() => {
     setTimeout(() => {
       setScreenLoading(false);
-    }, 3000);
+    }, 5000);
   }, []);
 
   return (
--- src/views/home/components/Header.jsx
+++ src/views/home/components/Header.jsx
@@ -28,38 +28,25 @@ const Header = () => {
             className="flex lg:flex-row-reverse flex-col lg:w-[87%] mx-auto overflow-hidden"
           >
             <div className="lg:w-1/2 relative h-[350px] lg:h-[950px]">
-              <img
-                src={urlIMG}
-                className="rounded-full absolute w-80 h-80 -left-24 -top-12 -z-20 lg:hidden"
-                width={320}
-              />
+              <div
+                className="lg:w-[586.086px] lg:h-[586.086px] w-80 h-80 rounded-full absolute lg:right-0 lg:left-8 -left-24 -top-12 -z-10"
+                style={{
+                  backgroundImage: `linear-gradient(155deg, #ff8487 5%, rgba(255,255,255,0) 75.32%), url(${urlIMG})`,
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                }}
+              ></div>
 
-              <div className="bg__img__header w-80 h-80 -z-10 -left-24 -top-12 lg:hidden"></div>
-
-              <img
-                src={urlIMG}
-                className="rounded-full w-[586.086px] h-[586.086px] absolute right-0 -top-12 -z-10 hidden lg:block"
-                alt=""
-              />
-
-              <div className="bg__img__header w-[586.086px] h-[586.086px] -z-10 right-0 -top-12 hidden lg:block"></div>
-
-              <img
-                src={urlIMG2}
-                className="rounded-full w-[367px] h-[367px] absolute right-0 bottom-0 -z-10 hidden lg:block"
-                alt=""
-              />
-
-              <div className="bg__img__header2 w-[367px] h-[367px] right-0 bottom-0 hidden lg:block"></div>
-
-              <img
-                src={urlIMG2}
-                className="rounded-full w-[200px] h-[200px] absolute -right-20 -bottom-10 -z-10 lg:hidden"
-                width={200}
-                alt=""
-              />
-
-              <div className="bg__img__header2 w-[200px] h-[200px] -right-20 -bottom-10 lg:hidden"></div>
+              <div
+                className="lg:w-[367px] lg:h-[367px] w-[200px] h-[200px] rounded-full absolute lg:right-0 lg:bottom-0 -right-20 -bottom-10 -z-10"
+                style={{
+                  backgroundImage: `linear-gradient(323deg, #FF5053 7.42%, rgba(255, 255, 255, 0.00) 52.36%), url(${urlIMG2})`,
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                }}
+              ></div>
             </div>
 
             <div className="lg:w-1/2 flex flex-col justify-center lg:gap-12 gap-9 w-[87%] mx-auto">
