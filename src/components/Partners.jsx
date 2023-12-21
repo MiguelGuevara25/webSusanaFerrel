a5b82881fbb32bb86bf48c628ea589a444bbb498
Miguel Guevara <miguel_g1789@hotmail.com>
2023-12-20 21:38:07 -0500

    cambios partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -9,24 +9,7 @@ const Partners = () => {
         const { numero_partners, titulo_partners, logo_partners } =
           datos.attributes;
 
-        const partner1 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[0].attributes.url;
-        const partner2 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[1].attributes.url;
-        const partner3 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[2].attributes.url;
-        const partner4 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[3].attributes.url;
-        const partner5 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[4].attributes.url;
-        const partner6 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[5].attributes.url;
-        const partner7 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[6].attributes.url;
-        const partner8 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[7].attributes.url;
-        const partner9 =
-          import.meta.env.VITE_IMG_URL + logo_partners.data[8].attributes.url;
+        const imgUrl = import.meta.env.VITE_IMG_URL;
 
         return (
           <section key={datos.id} className="text-[#024F3C]">
@@ -40,16 +23,17 @@ const Partners = () => {
             </div>
 
             <div className="relative">
-              <div className="grid lg:grid-cols-10 grid-cols-3 w-[87%] mx-auto place-items-center lg:gap-0 gap-6">
-                <img src={partner1} className="lg:col-span-2" />
-                <img src={partner2} className="lg:col-span-2" />
-                <img src={partner3} className="lg:col-span-2" />
-                <img src={partner4} className="lg:col-span-2" />
-                <img src={partner5} className="lg:col-span-2" />
-                <img src={partner6} className="lg:col-start-2 lg:col-end-4" />
-                <img src={partner7} className="lg:col-start-4 lg:col-end-6" />
-                <img src={partner8} className="lg:col-start-6 lg:col-end-8" />
-                <img src={partner9} className="lg:col-start-8 lg:col-end-10" />
+              <div className="max-w-screen-xl mx-auto flex flex-row flex-wrap justify-center py-20 gap-x-10 items-center">
+                {logo_partners.data.map((partner, index) => {
+                  return (
+                    <div className="basis-auto w-[90px] md:w-48" key={index}>
+                      <img
+                        src={`${imgUrl}${partner.attributes.url}`}
+                        alt={`Partner ${index + 1}`}
+                      />
+                    </div>
+                  );
+                })}
               </div>
 
               <img
